5eaa952864d2c7eb12bbcb95306faf07307fa20c
Marilena Sales <marilena.sales@gmail.com>
2021-07-13 19:27:12 -0300

    projeto final

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,41 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    const newTask ={
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    // ... todos os valores antigos
+    setTasks(oldState => [...oldState,newTask]);
+    setNewTaskTitle('');
   }
+
+
+
+
+
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    //verifica cada task mapeada 
+    const newTasks = tasks.map(task => task.id === id ?{
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+
+    setTasks(newTasks)
   }
+
+
+
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    //retorna todas as tasks que sejam diferentes do id
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks)
   }
 
   return (
